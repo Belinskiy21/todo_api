c86ef2e901fa14297d3c2798857ed60889c2e7af
Oleg <belinskiy21@gmail.com>
2018-01-11 23:33:16 +0200

    add uniqueness to user.email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :projects, foreign_key: :created_by
+  validates :email,uniqueness: true
   validates_presence_of :email, :password_digest
 end
