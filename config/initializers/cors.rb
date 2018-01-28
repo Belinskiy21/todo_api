51f8514fd7e1aa6e1f7caaeb1f3ae467c4683440
Oleg <belinskiy21@gmail.com>
2018-01-28 11:14:33 +0200

    change cors back to 3001

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001, https://rocky-shelf-60083.herokuapp.com'
+    origins 'localhost:3001'
 
     resource '*',
       headers: :any,
