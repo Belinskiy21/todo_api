5befd16ac235cf0405938c8d50c0ab0d4f0bf8c3
Oleg <belinskiy21@gmail.com>
2018-01-28 11:00:24 +0200

    change cors to *

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://rocky-shelf-60083.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
