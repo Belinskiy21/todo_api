01407b04a3a146c4c8ead66bb9e31a6fc9fe1239
Oleg <belinskiy21@gmail.com>
2018-01-28 10:25:44 +0200

    change cors config for heroku client

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'rocky-shelf-60083.herokuapp.com'
 
     resource '*',
       headers: :any,
