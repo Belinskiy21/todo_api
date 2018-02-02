2c33c2c8786f3ad3a642c8b6602ff62e6d11f0b9
Oleg <belinskiy21@gmail.com>
2018-02-02 20:57:40 +0200

    with apipie custom validator

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,3 +1,19 @@
+class FileValidator < Apipie::Validator::BaseValidator
+
+  def validate(value)
+    value.is_a?(Rack::Test::UploadedFile) || value.is_a?(ActionDispatch::Http::UploadedFile)
+  end
+
+  def self.build(param_description, argument, options, block)
+    self.new param_description if argument == File
+  end
+
+  def description
+    'Must be a valid file'
+  end
+
+end
+
 Apipie.configure do |config|
   config.app_name                = "TodoApi"
   config.api_base_url            = "/api/v1"
@@ -7,4 +23,5 @@ Apipie.configure do |config|
   config.translate               = false
   config.default_locale          = nil
   config.show_all_examples       = true
+  config.validate                =  FileValidator.new(File)
 end
