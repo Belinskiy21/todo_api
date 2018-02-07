43e16aa528d61b20b39649284c569d7453f2dccd
Oleg <belinskiy21@gmail.com>
2018-02-07 14:12:51 +0200

    fix task factory and comment.json schema

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
     text { Faker::Lorem.sentence }
-    file { Faker::File.file_name }
+    file {"file: credentials"}
     task_id nil
   end
 end
--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :task do
     title { Faker::StarWars.character }
     done false
+    deadline '11.12.2018'
     project_id nil
   end
 end
