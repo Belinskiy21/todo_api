fa0389a8090b9f0680c6107be97ed694905e49b8
Oleg <belinskiy21@gmail.com>
2018-02-08 00:22:11 +0200

    add @task instance to update

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -41,8 +41,8 @@ class Api::V1::TasksController < Api::V1::ApiController
   param :id, :number, required: true
   def update
     Task.change(@task, params[:move]) if params[:move]
-    @task.update(task_params)
-    head :no_content
+    @task = @task.update(task_params)
+    json_response(@task, :updated)
   end
 
   api :DELETE, '/projects/:project_id/tasks/:id', 'Desroy task by :id'
