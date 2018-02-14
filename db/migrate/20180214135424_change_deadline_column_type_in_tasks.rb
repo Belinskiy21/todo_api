b43dfd5765b66a4e8b987e648a8ed2433d27c814
Oleg <belinskiy21@gmail.com>
2018-02-14 16:31:25 +0200

    add string with symbol

--- db/migrate/20180214135424_change_deadline_column_type_in_tasks.rb
+++ db/migrate/20180214135424_change_deadline_column_type_in_tasks.rb
@@ -1,5 +1,5 @@
 class ChangeDeadlineColumnTypeInTasks < ActiveRecord::Migration[5.1]
   def change
-    change_column :tasks, :deadline, :datetime
+    change_column :tasks, :deadline, :datetime, "USING deadline::timestamp without time zone"
   end
 end
